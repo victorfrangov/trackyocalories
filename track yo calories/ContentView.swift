f64040f7a1bd9f73f4e9b19a7894e8d8eaafb4a9
Victor Frangov <victor.frangov@gmail.com>
2026-08-28 19:47:50 -0400

    Initial Commit

---
+++ track yo calories/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  track yo calories
+//
+//  Created by Victor Frangov on 2026-08-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ track yo calories/track_yo_caloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  track_yo_caloriesApp.swift
+//  track yo calories
+//
+//  Created by Victor Frangov on 2026-08-28.
+//
+
+import SwiftUI
+
+@main
+struct track_yo_caloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
